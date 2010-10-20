21c4227d3c5a995915f0471b76f3fa292fa850f1
David L Kinney <david@pinkhop.com>
2010-10-20 00:43:37 -0500

    Abstracted OAuth client

--- reflector-dish.js
+++ reflector-dish.js
@@ -1,5 +1,5 @@
 var sys           = require('sys')
-var oauth         = require('oauth-client')
+var oauth         = require('./lib/oauth_client.js')
 var authorization = require('./lib/authorization.js')
 var data          = require('./lib/data.js')
 var twitter       = require('./lib/twitter.js')
@@ -7,15 +7,16 @@ var twitter       = require('./lib/twitter.js')
 var ReflectorDish = function() {
   var self = this
   
-  self.restClient = oauth.createClient(80, 'api.twitter.com', false)
-  self.streamClient = oauth.createClient(80, 'stream.twitter.com', false)
   self.username = null
-  self.consumer = null
-  self.signer = null
-  self.access = null
+  self.restClient = null
+  self.streamClient = null
   
   self.init = function(username, onComplete) {
     self.username = username
+    
+    self.restClient = oauth.client('http://api.twitter.com/')
+    self.streamClient = oauth.client('http://stream.twitter.com/')
+    
     self._loadConsumerTokens(function() {
       self._loadAccessTokens(onComplete)
     })
@@ -23,7 +24,8 @@ var ReflectorDish = function() {
   
   self._loadConsumerTokens = function(onComplete) {
     data.loadConsumerTokens(function(consumerKey, consumerSecret) {
-      self.consumer = oauth.createConsumer(consumerKey, consumerSecret);
+      self.restClient.setConsumer(consumerKey, consumerSecret)
+      self.streamClient.setConsumer(consumerKey, consumerSecret)
       if (onComplete) onComplete()
     })
   }
@@ -32,42 +34,28 @@ var ReflectorDish = function() {
     authorization.loadAccessTokens({
       username: self.username
     , client: self.client
-    , callback: function(token, secret) {
-        self.access = oauth.createToken(token, secret)
-        self.signer = oauth.createHmac(self.consumer, self.access)
+    , callback: function(accessToken, accessTokenSecret) {
+        self.restClient.setAccess(accessToken, accessTokenSecret)
+        self.streamClient.setAccess(accessToken, accessTokenSecret)
         if (onComplete) onComplete()
       }
     })
   }
   
-  self.get = function(options) {
-    request = self.restClient.request('GET', options.uri, options.headers, null, self.signer)
-    request.addListener('response', function(response) {
-      var data = []
-      response.addListener('data', function(chunk) {
-        data.push(chunk)
-      })
-      response.addListener('end', function() {
-        options.callback(response.statusCode, response.headers, data.join(''))
-      })
-    })
-    request.end()
-  }
-  
   self.getMembersOfList = function(listURI, callback) {
-    var members = []
-    var onData = function(membersOnPage) {
-      membersOnPage.forEach(function(member) {
-        members.push(member)
+    var allMembers = []
+    var onMembers = function(members) {
+      members.forEach(function(member) {
+        allMembers.push(member)
       })
     }
     var onComplete = function() {
-      callback(members)
+      callback(allMembers)
     }
     
     self._getMembersOfList({
       listURI: listURI
-    , data: onData
+    , members: onMembers
     , complete: onComplete
     })
   }
@@ -75,31 +63,33 @@ var ReflectorDish = function() {
   self._getMembersOfList = function(options) {
     var listURI    = options.listURI.replace(/^[@/]/, '')
     var cursor     = options.cursor || -1
-    var onData     = options.data
+    var onMembers  = options.members
     var onComplete = options.complete
     
     var uri = "http://api.twitter.com/1/" + listURI + "/members.json?cursor=" + cursor
-    var handler = function(statusCode, headers, body) {
-      if (statusCode != 200) throw "Unable to get member list: " + statusCode + " " + JSON.stringify(headers)
-      
+    var err = function(response) {
+      if (response) throw new Error(sys.inspect(response))
+    }
+    var success = function(body) {
       var response = JSON.parse(body)
-      var membersOnPage = response.users
-      var nextCursor = response.next_cursor
-      onData(membersOnPage)
       
+      var membersOnPage = response.users
+      onMembers(membersOnPage)
+      
+      var nextCursor = response.next_cursor
       if (nextCursor > 0) {
-        self._getMembersOfList({
-          listURI:  listURI
-        , cursor:   nextCursor
-        , data:     onData
-        , complete: onComplete
-        })
+        var nextOptions = Object.merge(options, { cursor: nextCursor })
+        self._getMembersOfList(nextOptions)
       } else {
         onComplete()
       }
     }
     
-    self.get({ uri: uri, callback: handler })
+    self.restClient.get({ 
+      uri: uri
+    , error: err
+    , complete: success
+    })
   }
   
 }
